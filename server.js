9161e0890317dd0c01bf84d7b57f59b6d7e7d7a3
Jayden <S10267761@connect.np.edu.sg>
2025-07-13 17:27:34 +0800

    implemented jwt authentication

--- server.js
+++ server.js
@@ -32,7 +32,24 @@ app.get('/posts', authenticateToken, (req, res) => {
   res.json(posts.filter(post => post.username === req.user.name))
 })
 
+let refreshTokens = []
+
+app.post('/token', (req,res) => {
+  const refreshToken = req.body.token
+  if (refreshToken == null ) return res.sendStatus(401)
+  if (refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process,env.REFRESH_TOKEN_SECRET, (err,user) => {
+    if (err) return res.sendStatus(403)
+      const accessToken = generateAccessToken({ name: user.name })
+    res.json({ accessToken: accessToken })
+  })
+})
+
 //Authentication 
+app.delete('/logout', (req,res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  res.sendStatus(204)
+})
 app.post('/login',(req,res) => {
   // Authenticate User
 
